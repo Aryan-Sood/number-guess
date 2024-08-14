9b4cc2854d2a18fcf6f6e2a561f3dd0487333004
Aryan-Sood <aryansood12@gmail.com>
2024-08-15 02:32:12 +0530

    some work on start game screen

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,37 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function PrimaryButton({children}){
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.ButtonOuterContainer}>
+        <Pressable style={({pressed}) => pressed ? [styles.ButtonInnerContainer, pressed] : styles.ButtonInnerContainer} android_ripple={{color:'#640233'}}>
+            <Text style={styles.buttonText}>{children}</Text>
+        </Pressable>
         </View>
     );
 }
 
 export default PrimaryButton;
+
+
+const styles = StyleSheet.create({
+
+    buttonOuterContainer:{
+        borderRadius:28,
+        margin:4,
+        overflow:"hidden"
+    },
+    ButtonInnerContainer:{
+        backgroundColor:'#72063c',
+        paddingVertical:8,
+        paddingHorizontal:16,
+        elevation:2,
+        marginVertical:5,
+    },
+    buttonText:{
+        color:'white',
+        textAlign:"center"
+    },
+    pressed:{
+        opacity:0.75
+    }
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -8,9 +8,15 @@ function StartGameScreen(){
     <>
     <StatusBar style="inverted"/>
     <View style={styles.inputContainer}>
-        <TextInput/>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+        <TextInput style = {styles.numberInput} maxLength={2} keyboardType="number-pad" autoCorrect={false}/>
+        <View style = {styles.buttonsContainer}>
+            <View style={styles.buttonContainer}>
+            <PrimaryButton>Reset</PrimaryButton>
+            </View>
+            <View style={styles.buttonContainer}>
+            <PrimaryButton>Confirm</PrimaryButton>
+            </View>
+        </View>
     </View>
     </>
     );
@@ -22,15 +28,37 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
     inputContainer:{
+      justifyContent:'center',
+      alignItems:'center',
       padding: 16,
       marginTop:100,
       marginHorizontal:24,
-      backgroundColor:'#72063c',
+      backgroundColor:'#3b021f',
       borderRadius:8,
       elevation:4,
       shadowColor:'black',
       shadowOffset:{width: 0, height:2},
       shadowRadius:6,
       shadowOpacity:0.5
+    },
+    numberInput:{
+        height:50,
+        width:50,
+        textAlign:"center",
+        
+        fontSize:32,
+        borderBottomColor:'#ddb52f',
+        borderBottomWidth:2,
+        color: '#ddb52f',
+        marginVertical:8,
+        fontWeight:'bold'
+    },
+    buttonsContainer:{
+        flexDirection:"row",
+    },
+    buttonContainer:{
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
     }
   })
